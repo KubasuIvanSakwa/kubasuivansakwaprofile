db27c57a07418c7f2b03b7fa9f1a9bdad888a71b
KubasuIvanSakwa <kubasuivansakwa23@gmail.com>
2024-05-12 21:02:09 +0300

    updates

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,11 @@
+import { useOutletContext } from "react-router-dom"
 
 function Hero() {
+    const [ tog ] = useOutletContext();
+    console.log(tog)
 
     return (
-        <section className="text-white bg-blue-300/40">
+        <section className={`${tog === 'd' ? 'text-white' : 'text-black'} bg-blue-300/40`}>
             Hero
         </section>
     )
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,36 +2,16 @@ import { useEffect, useRef, useState } from "react"
 import { Outlet } from "react-router-dom"
 
 function Layout() {
-    const [tog, setTog] = useState('light theme')
+    const [tog, setTog] = useState('dark theme')
     const theme = useRef(null)
-    const themeBtn = useRef(null)
 
     useEffect(() => {
+        
         window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', function(e) { 
             const newColorScheme = e.matches ? 'dark' : 'light'
             newColorScheme === 'dark' ? setTog('dark theme') : setTog('light theme')
-            console.log(newColorScheme)
-            console.log(tog)
         })
     })
-
-    function handleTheme() {
-        if(tog === 'dark theme') {
-            theme.current.classList.remove('mainpglight')
-            theme.current.classList.add('mainpgdark')
-        }else {
-            theme.current.classList.remove('mainpgdark')
-            theme.current.classList.add('mainpglight')
-        }
-    }
-
-    function handleTog() {
-        setTog((prevTog) => (
-            prevTog === 'light theme' ?  'dark theme' : 'light theme'
-        ))
-        handleTheme()
-    }
-
 
     return (
         <section className={`${tog === 'dark theme' ? 'mainpgdark' : 'mainpglight'} ease-in w-full flex justify-center h-[100vh] overflow-hidden`} ref={theme}>
@@ -45,15 +25,9 @@ function Layout() {
                                 <span className="button__icon"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 35 35" id="bdd05811-e15d-428c-bb53-8661459f9307" data-name="Layer 2" className="svg"><path d="M17.5,22.131a1.249,1.249,0,0,1-1.25-1.25V2.187a1.25,1.25,0,0,1,2.5,0V20.881A1.25,1.25,0,0,1,17.5,22.131Z"></path><path d="M17.5,22.693a3.189,3.189,0,0,1-2.262-.936L8.487,15.006a1.249,1.249,0,0,1,1.767-1.767l6.751,6.751a.7.7,0,0,0,.99,0l6.751-6.751a1.25,1.25,0,0,1,1.768,1.767l-6.752,6.751A3.191,3.191,0,0,1,17.5,22.693Z"></path><path d="M31.436,34.063H3.564A3.318,3.318,0,0,1,.25,30.749V22.011a1.25,1.25,0,0,1,2.5,0v8.738a.815.815,0,0,0,.814.814H31.436a.815.815,0,0,0,.814-.814V22.011a1.25,1.25,0,1,1,2.5,0v8.738A3.318,3.318,0,0,1,31.436,34.063Z"></path></svg></span>
                             </a>
                         </div>
-                        <div className="toggle-switch mr-2 opacity-70 relative right-[-0.3rem]">
-                            <label className="switch-label">
-                                <input type="checkbox" className="checkbox" onClick={handleTog} ref={themeBtn}/>
-                                    <span className={`${tog === 'dark theme' ? 'slider-1' : 'slider-2'} slider`}></span>
-                            </label>
-                        </div> 
                     </div>
                 </nav>
-                <Outlet />
+                <Outlet context={tog}/>
             </section>
         </section>
     )

1281550787fc4f4fd2c725d616f1c24e2e6e8136
KubasuIvanSakwa <kubasuivansakwa23@gmail.com>
2024-05-21 15:42:27 +0300

    ui update hero section theme toggler

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,24 @@
 import { useOutletContext } from "react-router-dom"
 
 function Hero() {
-    const [ tog ] = useOutletContext();
-    console.log(tog)
+    const [ tog ] = useOutletContext()
 
     return (
-        <section className={`${tog === 'd' ? 'text-white' : 'text-black'} bg-blue-300/40`}>
-            Hero
+        <section className={`${tog === 'd' ? 'text-white' : 'text-black'} flex justify-end mt-10`}>
+            <section className="flex w-[15rem] h-[6.4rem] rounded-[18px] p-2 m-2 bg-[#A9A9A9]/40">
+                <div className=" rounded-[18px] w-[2.5rem] flex justify-center items-center ">
+                    <p className={`${tog === 'l' ? 'text-[#252525]' : 'text-[#dad5d5]'} text-4xl pacifico-regular`}>
+                        I
+                        <span className="text-sm">ntro</span>
+                    </p>
+                </div>
+                <div className="ml-3 flex-col h-[4rem] w-[10rem] top-[0.1rem] relative pacifico-regular">
+                    <p className={`${tog === 'l' ? 'text-[#252525]' : 'text-[#dad5d5]'} text-[0.875rem] leading-3 `}>Software engineer</p>
+                    <p className="leading-6 text-[2rem] text-[#a5a5a5] relative z-10">Kubasu</p>
+                    <p className="leading-6 text-[2rem] text-[#b36464] relative z-30">Ivan</p>
+                    <p className="leading-6 text-[2rem] text-[#a5a5a5] relative z-40">Sakwa</p>
+                </div>
+            </section>
         </section>
     )
 }
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,11 +2,28 @@ import { useEffect, useRef, useState } from "react"
 import { Outlet } from "react-router-dom"
 
 function Layout() {
+    const themeRef = useRef(null)
     const [tog, setTog] = useState('dark theme')
     const theme = useRef(null)
 
+    function handleChange(){
+        if(themeRef.current.checked) {
+            console.log('checked')
+            setTog('dark theme')
+        }else {
+            console.log('unchecked')
+            setTog('light theme')
+        }
+    }
+
     useEffect(() => {
-        
+        if(tog === 'dark theme') {
+            themeRef.current.checked = true
+        }
+        else if(tog === 'light theme') {
+            themeRef.current.checked = false
+        }
+
         window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', function(e) { 
             const newColorScheme = e.matches ? 'dark' : 'light'
             newColorScheme === 'dark' ? setTog('dark theme') : setTog('light theme')
@@ -15,17 +32,20 @@ function Layout() {
 
     return (
         <section className={`${tog === 'dark theme' ? 'mainpgdark' : 'mainpglight'} ease-in w-full flex justify-center h-[100vh] overflow-hidden`} ref={theme}>
-            <section className="backdrop-blur-sm shadow-xl w-[65vw] h-fit overflow-y-scroll p-2 relative z-30">
-                <nav className='w-full rounded-full h-[3rem] pl-2 flex items-center relative border border-slate-500/20 justify-end relative'>
+            <section className="backdrop-blur-sm shadow-xl w-[65vw] h-fit overflow-y-scroll p-2">
+                <nav className='w-full rounded-full h-[3rem] pl-2 flex items-center border border-slate-500/20 justify-end'>
                     <div className="flex justify-between items-center w-full h-fit">
                         <div className="flex items-center h-fit justify-between w-full relative left-[-0.7rem]">
                             <div className="bg-[url('/src/assets/me.png')] w-[2.5rem] h-[2.5rem] bg-cover rounded-full mr-2 ml-2 opacity-70"></div>
-                            <a  href="/src/assets/KubasuIvanSakwa.pdf" 
-                                className={`border ${tog === 'dark theme' ? 'border-white/50 text-white/50' : 'border-black/50 text-black/50'} relative  w-[6rem] rounded-r-full right-[-0.4rem] h-[2.4rem] flex items-center justify-center p-1 rounded`} 
-                                download
-                            >
-                                <span className="">resume</span>
-                            </a>
+                            <div className="flex justify-between w-[11rem]">
+                                <input title="change-theme" type="checkbox" onClick={handleChange} className="l cursor-pointer" ref={themeRef}></input>
+                                <a  href="/src/assets/KubasuIvanSakwa.pdf" 
+                                    className={`border ${tog === 'dark theme' ? 'border-white/50 text-white/50' : 'border-black/50 text-black/50'} relative resume w-[6rem] rounded-r-full right-[-0.4rem] h-[2.4rem] flex items-center justify-center p-1 rounded`} 
+                                    download
+                                >   
+                                    <span className="">resume</span>
+                                </a>
+                            </div>
                         </div>
                     </div>
                 </nav>

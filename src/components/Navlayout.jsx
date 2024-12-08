c5c79fc8e75d6afd76aa977d85d63237610ebf91
Kubasu Ivan Sakwa <72573043+KubasuIvanSakwa@users.noreply.github.com>
2024-12-08 22:14:04 +0300

    Update Navlayout.jsx

--- src/components/Navlayout.jsx
+++ src/components/Navlayout.jsx
@@ -1,9 +1,9 @@
 import { Link } from 'react-router-dom'
 
-function Navlayout({ img1, img2, url1, url2, tog, w }) {
+function Navlayout({ img1, img2, url1, url2, tog }) {
 
     return (
-        <section className={`${tog === 'd' ? 'bg-[#1b1b1b]' : 'bg-[#1b1b1b]/80'} fixed border border-white/10 flex flex-col gap-[2rem] justify-center h-[8.8rem] w-[${w}] rounded-full`}>
+        <section className={`${tog === 'd' ? 'bg-[#1b1b1b]' : 'bg-[#1b1b1b]/80'} fixed border border-white/10 flex flex-col gap-[2rem] justify-center h-[8.8rem] w-[4%] rounded-full`}>
             {!url1.includes('http') && <Link
                 to={url1}
                 className='p-3 w-full h-fit rounded-full hover:bg-[#202020]'
